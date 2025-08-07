6632dc37e5f78f79234a4eefc49d2238c0578d7c
pornpavee-b <ppvsysr2549@gmail.com>
2025-08-07 08:01:52 +0000

    tryok

---
+++ try.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int integer1;
+    int integer2;
+    int integer3;
+    char ch;
+    int max = integer3;
+    int min = integer2 ;
+ 
+    printf("Enter integer#1 :");
+    scanf("%d%c", &integer1,&ch);
+    printf("Enter integer#2 :\n");
+    scanf("%d%c",&integer2,&ch);
+    printf("Enter integer#3 :\n");
+    scanf(" %d%c",&integer3,&ch);
+    
+    printf("Maximum:%d\n",max);
+    printf("Minimum:%d",min);
+ 
+     
+
+
+
+
+    
+     
+
+    return(0);
+}
